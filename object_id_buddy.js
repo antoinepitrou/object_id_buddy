11bfc8fa5cc3bb13566b1d7eb03c703ce9466373
Antoine Pitrou <pitrou.antoine@gmail.com>
2016-12-21 18:09:29 -0800

    create the javascript

---
+++ object_id_buddy.js
@@ -0,0 +1,22 @@
+function addObjectIdWrapper(text) {
+
+  var re = new RegExp(/[a-f\d]{24}/, 'ig');
+  return text.replace(re, "ObjectId(\"$&\")");
+
+}
+
+function getText() {
+  return document.getElementById("object-id-content").value;
+}
+
+function jamItHome(output) {
+  document.getElementById("object-id-output").innerHTML = output;
+}
+
+document.getElementById("object-id-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  var input = getText();
+  var output = addObjectIdWrapper(input);
+
+  jamItHome(output);
+})
